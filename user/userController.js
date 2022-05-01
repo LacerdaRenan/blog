133025cd0a30548121b638e8a8882e3e1f17b445
RenanLacerda <renanrodlacerda@gmail.com>
2022-04-30 23:03:09 -0300

    criando pag de listagem de usuários

--- user/userController.js
+++ user/userController.js
@@ -1,10 +1,15 @@
 const {Router} = require('express');
 const User = require('./user');
+const bcrypt = require('bcryptjs');
 
 let router = Router();
 
 router.get('/admin/users', (req,res)=>{
-    res.send('Creating a router, so a user can login');
+    User.findAll().then(users=>{
+        res.render('admin/users/index.ejs',{
+            users:users
+        })
+    })
 })
 
 router.get('/admin/user/create', (req,res)=>{
@@ -15,14 +20,26 @@ router.post('/admin/user/new', (req,res)=>{
     let email = req.body.email;
     let password = req.body.password;
     
-    res.json({email, password});
-    
-    User.create({
-        email:email,
-        password:password
-    }).then(()=>{
-        res.redirect('/');
+    User.findOne({
+        where:{
+            email:email
+        }
+    }).then(u=>{
+        if(u==undefined){
+            let salt = bcrypt.genSaltSync(10);
+            let hash = bcrypt.hashSync(password, salt)
+        
+            User.create({
+                email:email,
+                password:hash
+            }).then(()=>{
+                res.redirect('/');
+            })
+        }else{
+            res.send('nn')
+        }
     })
+    
 
 })
 
